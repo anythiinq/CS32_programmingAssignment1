563421b54d39195a1c54261c366eb72e694c3643
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-01-08 17:10:12 -0800

    Refactor History class to include private members

--- History.h
+++ History.h
@@ -3,11 +3,14 @@
 #define HISTORY_H
 
 class History
-	{
-	  public:
-	    History(int nRows, int nCols);
-	    bool record(int r, int c);
-	    void display() const;
-	};
+{
+	public:
+		History(int nRows, int nCols);
+		bool record(int r, int c);
+		void display() const;
+	private: 
+		int m_rows;
+		int m_cols;
+};
 
 #endif
