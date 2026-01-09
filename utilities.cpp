d1065d99bffce7a61a49131dab04fa22e8fabc04
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-01-08 16:36:57 -0800

    Remove decodeDirection and implement clearScreen
    Removed decodeDirection function and added clearScreen implementation.

--- utilities.cpp
+++ utilities.cpp
@@ -18,19 +18,6 @@ int randInt(int lowest, int highest)
     return distro(generator);
 }
 
-bool decodeDirection(char ch, int& dir)
-{
-    switch (tolower(ch))
-    {
-      default:  return false;
-      case 'n': dir = NORTH; break;
-      case 'e': dir = EAST;  break;
-      case 's': dir = SOUTH; break;
-      case 'w': dir = WEST;  break;
-    }
-    return true;
-}
-
   // Return false without changing anything if moving one step from (r,c)
   // in the indicated direction would run off the edge of the arena.
   // Otherwise, update r and c to the position resulting from the move and
@@ -114,3 +101,55 @@ int computeDanger(const Arena& a, int r, int c)
 
     return danger;
 }
+
+///////////////////////////////////////////////////////////////////////////
+//  clearScreen implementation
+///////////////////////////////////////////////////////////////////////////
+
+// YOU MAY MOVE TO ANOTHER FILE THE CODE FROM HERE TO THE END OF FILE, BUT
+// BE SURE TO MOVE *ALL* THE CODE, AND DO NOT CHANGE IT IN ANY WAY; DON'T
+// MODIFY OR REMOVE ANY #IFDEF, ETC.  THE CODE IS SUITABLE FOR VISUAL C++,
+// XCODE, AND g++/g31/g32 UNDER LINUX.
+
+// Note to Xcode users:  clearScreen() will just write a newline instead
+// of clearing the window if you launch your program from within Xcode.
+// That's acceptable.  (The Xcode output window doesn't have the capability
+// of being cleared.)
+
+#ifdef _WIN32
+
+#pragma warning(disable : 4005)  // redefined macro in <windows.h>
+#include <windows.h>
+
+void clearScreen()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    DWORD dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+    COORD upperLeft = { 0, 0 };
+    DWORD dwCharsWritten;
+    FillConsoleOutputCharacter(hConsole, TCHAR(' '), dwConSize, upperLeft,
+                                                        &dwCharsWritten);
+    SetConsoleCursorPosition(hConsole, upperLeft);
+}
+
+#else  // not _WIN32
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+void clearScreen()  // will just write a newline in an Xcode output window
+{
+    static const char* term = std::getenv("TERM");
+    if (term == nullptr  ||  std::strcmp(term, "dumb") == 0)
+        std::cout << std::endl;
+    else
+    {
+        static const char* ESC_SEQ = "\x1B[";  // ANSI Terminal esc seq:  ESC [
+        std::cout << ESC_SEQ << "2J" << ESC_SEQ << "H" << std::flush;
+    }
+}
+
+#endif
