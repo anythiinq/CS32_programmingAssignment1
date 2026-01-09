246fd8fdb9075c70818726954055bf98c1a42d2e
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-01-08 16:44:29 -0800

    Remove recommendMove function from utilities.cpp
    Removed the recommendMove function that evaluated player movement options based on danger levels.

--- utilities.cpp
+++ utilities.cpp
@@ -38,48 +38,6 @@ bool attemptMove(const Arena& a, int dir, int& r, int& c)
     return true;
 }
 
-  // Recommend a move for a player at (r,c):  A false return means the
-  // recommendation is that the player should drop a poisoned brain and
-  // not move; otherwise, this function sets bestDir to the recommended
-  // direction to move and returns true.
-bool recommendMove(const Arena& a, int r, int c, int& bestDir)
-{
-      // How dangerous is it to stand?
-    int standDanger = computeDanger(a, r, c);
-
-      // if it's not safe, see if moving is safer
-    if (standDanger > 0)
-    {
-        int bestMoveDanger = standDanger;
-        int bestMoveDir = NORTH;  // arbitrary initialization
-
-          // check the four directions to see if any move is
-          // better than standing, and if so, record the best
-        for (int dir = 0; dir < NUMDIRS; dir++)
-        {
-            int rnew = r;
-            int cnew = c;
-            if (attemptMove(a, dir, rnew, cnew))
-            {
-                int danger = computeDanger(a, rnew, cnew);
-                if (danger < bestMoveDanger)
-                {
-                    bestMoveDanger = danger;
-                    bestMoveDir = dir;
-                }
-            }
-        }
-
-          // if moving is better than standing, recommend move
-        if (bestMoveDanger < standDanger)
-        {
-            bestDir = bestMoveDir;
-            return true;
-        }
-    }
-    return false;  // recommend standing
-}
-
 int computeDanger(const Arena& a, int r, int c)
 {
       // Our measure of danger will be the number of zombies that might move
