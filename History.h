39ea8888223c386224ba7d62fb29258c5d1f0476
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-01-08 17:02:39 -0800

    Add History class declaration in History.h

--- History.h
+++ History.h
@@ -1,1 +1,13 @@
+// NOT DONE YET I THINK
+#ifndef HISTORY_H
+#define HISTORY_H
 
+class History
+	{
+	  public:
+	    History(int nRows, int nCols);
+	    bool record(int r, int c);
+	    void display() const;
+	};
+
+#endif
